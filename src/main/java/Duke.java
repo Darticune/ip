d6536afc573c1f8799e51d30441370858ef4481a
Darticune <lucastan@DESKTOP-5V1LNS0.localdomain>
2020-08-28 00:36:12 +0800

    Update Duke.java to Level 3, Add Task.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,21 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static int addToList (String[] recordedList, String command, int listCount) {
-        recordedList[listCount] = command;
+    public static int addToList (Task[] recordedList, String command, int listCount) {
+        recordedList[listCount] = new Task(command);
         listCount++;
         System.out.println("added: " + command);
         return listCount;
     }
-    public static void printList (String[] recordedList, int listCount) {
+    public static void printList (Task[] recordedList, int listCount) {
         for (int i=0; i<listCount; i++) {
             System.out.print(i+1);
-            System.out.println(". " + recordedList[i]);
+            System.out.print(". ");
+            printTask(recordedList, i);
         }
+    }
+    public static void printTask (Task[] recordedList, int index) {
+        System.out.println("[" +recordedList[index].getStatusIcon() + "] " + recordedList[index].description);
     }
     public static void bidGoodbye () {
         printHorizontal();
@@ -19,7 +23,7 @@ public class Duke {
         printHorizontal();
     }
     public static void processCommand () {
-        String[] recordedList = new String[100];
+        Task[] recordedList = new Task[100];
         int listCount = 0;
         Scanner input = new Scanner(System.in);
         String command;
@@ -37,9 +41,21 @@ public class Duke {
                 printHorizontal();
                 break;
             default:
-                printHorizontal();
-                listCount = addToList(recordedList, command, listCount);
-                printHorizontal();
+                //Complete tasks
+                if((command.length() == 6) && ((command.startsWith("done")))) {
+                    int index = Integer.parseInt(command.substring(5));
+                    recordedList[index-1].setAsDone();
+                    printHorizontal();
+                    System.out.println("Nice! I've marked this task as done:");
+                    printTask(recordedList, (index-1));
+                    printHorizontal();
+                }
+                //Add tasks
+                else {
+                    printHorizontal();
+                    listCount = addToList(recordedList, command, listCount);
+                    printHorizontal();
+                }
             }
             command = input.nextLine();
         }
