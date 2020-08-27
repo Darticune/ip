adcc7adc8ec1e22e144ef3dd9d9b1216074ad15a
Darticune <lucastan@DESKTOP-5V1LNS0.localdomain>
2020-08-27 22:49:47 +0800

    Update Duke.java to Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void processCommand () {
+        Scanner input = new Scanner(System.in);
+        String command;
+        command = input.nextLine();
+
+        switch(command) {
+        case "bye":
+            printHorizontal();
+            System.out.println("Bye. Hope to see you again soon!");
+            printHorizontal();
+            break;
+        default:
+            printHorizontal();
+            System.out.println(command);
+            printHorizontal();
+            processCommand();
+        }
+
+    }
     public static void printHorizontal () {
         System.out.println("____________________________________________________________");
     }
     public static void main(String[] args) {
         printHorizontal();
+
+        printHorizontal();
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         printHorizontal();
-        System.out.println("Bye. Hope to see you again soon!");
-        printHorizontal();
+
+        processCommand();
     }
-}
+}   
