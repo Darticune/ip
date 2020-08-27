d3449c6f609a2fe2d1f77797d7d8660355de027f
Darticune <lucastan@DESKTOP-5V1LNS0.localdomain>
2020-08-27 23:22:43 +0800

    Update Duke.java to Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,47 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static int addToList (String[] recordedList, String command, int listCount) {
+        recordedList[listCount] = command;
+        listCount++;
+        System.out.println("added: " + command);
+        return listCount;
+    }
+    public static void printList (String[] recordedList, int listCount) {
+        for (int i=0; i<listCount; i++) {
+            System.out.print(i+1);
+            System.out.println(". " + recordedList[i]);
+        }
+    }
+    public static void bidGoodbye () {
+        printHorizontal();
+        System.out.println("Bye. Hope to see you again soon!");
+        printHorizontal();
+    }
     public static void processCommand () {
+        String[] recordedList = new String[100];
+        int listCount = 0;
         Scanner input = new Scanner(System.in);
         String command;
         command = input.nextLine();
 
-        switch(command) {
-        case "bye":
-            printHorizontal();
-            System.out.println("Bye. Hope to see you again soon!");
-            printHorizontal();
-            break;
-        default:
-            printHorizontal();
-            System.out.println(command);
-            printHorizontal();
-            processCommand();
+        while (true) {
+            if (command.equals("bye")) {
+                bidGoodbye();
+                break;
+            }
+            switch(command) {
+            case "list":
+                printHorizontal();
+                printList(recordedList, listCount);
+                printHorizontal();
+                break;
+            default:
+                printHorizontal();
+                listCount = addToList(recordedList, command, listCount);
+                printHorizontal();
+            }
+            command = input.nextLine();
         }
 
     }
@@ -24,7 +49,6 @@ public class Duke {
         System.out.println("____________________________________________________________");
     }
     public static void main(String[] args) {
-        printHorizontal();
 
         printHorizontal();
         System.out.println("Hello! I'm Duke");
